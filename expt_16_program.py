c0466f535739fa98bb27ba032d214ca88ddbf454
James D. D. Martin <jddmartin@uwaterloo.ca>
2023-06-12 12:46:14 -0400

    Add axes labelling etc..

--- expt_16_program.py
+++ expt_16_program.py
@@ -25,8 +25,8 @@ def data_directory():
 ##############################################################################
 # collect data using the NI USB-6008 DAQ
 ##############################################################################
-num_samples = 1000
-sample_rate = 5e3
+num_samples = 10000
+sample_rate = 5e3  # (Hz) do not change, maximum available
 with nidaqmx.Task() as task:
     task.ai_channels.add_ai_voltage_chan("Dev1/ai0",max_val=10, min_val=-10,
                                          terminal_config = TerminalConfiguration.DIFF)
@@ -50,7 +50,10 @@ fig,axs = plt.subplots(2)
 
 times = np.linspace(0, (num_samples-1)/sample_rate, num_samples)
 
-axs[0].plot(times, value[1])
+max_to_plot = 250
+axs[0].plot(times[:max_to_plot], value[1][:max_to_plot])
+axs[0].set_title("First few cycles")
+axs[0].set_xlabel("time (s)")
 
 data = value[1]
 
@@ -69,6 +72,9 @@ freqs = np.fft.fftfreq(len(data), time_step)
 idx = np.argsort(freqs)
 
 axs[1].plot(freqs[idx], amps[idx])
+axs[1].set_title("Power spectrum")
+axs[1].set_xlabel("frequency (Hz)")
+plt.tight_layout()
 np.savetxt(s+"_freq_domain.txt", np.c_[freqs[idx], amps[idx]])
 
 print("data saved with precursor filename: ", s)
